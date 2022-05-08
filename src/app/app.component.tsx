f4fabf044595e4a4c31cb902cd974febb6d06822
m-zs <mzasada93@gmail.com>
2022-05-08 13:22:14 +0200

    chore: setup styled-components

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "src/styles";
 
 export const App: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
