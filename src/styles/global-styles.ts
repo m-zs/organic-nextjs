53683c352c06f6deefe7b18b1bcd166300c70f1e
m-zs <mzasada93@gmail.com>
2022-05-08 18:31:20 +0200

    feat: add fonts

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from "styled-components";
+import { theme } from "./theme";
 
-export const GlobalStyles = createGlobalStyle`
-  body {
-    padding: 0;
-    margin: 0;
-  }
+export const GlobalStyles = createGlobalStyle<{ theme: typeof theme }>`
+  ${({ theme }) => `
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: ${theme.fonts.main};
+    }
+  `}
 `;
