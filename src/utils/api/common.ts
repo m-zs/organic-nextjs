4a5239cae5b068d9d3ababcfdd47ab62c8113468
m-zs <mzasada93@gmail.com>
2022-05-10 21:16:40 +0200

    refactor: remove console log

--- src/utils/api/common.ts
+++ src/utils/api/common.ts
@@ -2,8 +2,6 @@ export const buildBaseUrl = (url: string): string =>
   `${process.env.API_URL}/${url}`;
 
 export const fetchFromCMS = async <T>(url: string): Promise<T> => {
-  console.log(buildBaseUrl(url));
-
   const res = await fetch(buildBaseUrl(url), {
     headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
   });
