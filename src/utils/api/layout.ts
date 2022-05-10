fcd05396dfe065c427ab0fac99ba5457414e123b
m-zs <mzasada93@gmail.com>
2022-05-10 21:28:47 +0200

    chore: add layout comment

--- src/utils/api/layout.ts
+++ src/utils/api/layout.ts
@@ -3,6 +3,8 @@ import { Layout } from "src/types/app";
 import { Locale } from "src/types/internalization";
 import { fetchFromCMS } from "./common";
 
+// https://github.com/vercel/next.js/discussions/10949#discussioncomment-958694
+// shared data cant be fetched from _app right now
 export const fetchLayoutData = async (locale: Locale): Promise<Layout> => {
   const query = qs.stringify({
     populate: ["*"],
