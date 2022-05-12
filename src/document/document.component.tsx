2907a43e193a4a69ac2bfce98f1f3e2a2f4f1886
m-zs <mzasada93@gmail.com>
2022-05-12 19:49:02 +0200

    fix: remove double import

--- src/document/document.component.tsx
+++ src/document/document.component.tsx
@@ -6,11 +6,7 @@ export const Document = () => (
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Open+Sans:wght@300;400&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Open+Sans:wght@300;400&display=swap"
         rel="stylesheet"
       />
     </Head>
