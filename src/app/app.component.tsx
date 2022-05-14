c0c1de611234165c717b77d225bad1f38627d8ce
m-zs <mzasada93@gmail.com>
2022-05-14 19:44:55 +0200

    feat: add internalization provider

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,11 +1,30 @@
-import type { AppProps } from "next/app";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, theme } from "src/styles";
+import { LocalizationProvider } from "src/context/localization";
+import { Locale } from "src/types/internalization";
+import en from "src/content/translations/en.json";
+import pl from "src/content/translations/pl.json";
 
-export const App: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const messages = {
+  en,
+  pl,
+};
+
+export const App: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const { locale } = useRouter();
+
+  return (
+    <LocalizationProvider
+      locale={locale!}
+      messages={messages[locale as Locale]}
+    >
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps.page} />
+      </ThemeProvider>
+    </LocalizationProvider>
+  );
+};
