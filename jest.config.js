0e57d010b727efd2ba37303b9037ba69ddc2e08a
m-zs <mzasada93@gmail.com>
2022-05-17 20:43:43 +0200

    chore: add svg mock

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,9 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
   testPathIgnorePatterns: ["<rootDir>/cypress"],
+  moduleNameMapper: {
+    "\\.svg": "<rootDir>/__mocks__/svg.jsx",
+  },
 };
 
 export default createJestConfig(customJestConfig);
