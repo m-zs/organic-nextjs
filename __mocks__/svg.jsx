f302e5dd554380f515026035a83607afbc63be85
m-zs <mzasada93@gmail.com>
2022-05-17 20:43:43 +0200

    chore: add svg mock

---
+++ __mocks__/svg.jsx
@@ -0,0 +1,6 @@
+import { forwardRef } from "react";
+
+const SvgrMock = forwardRef((props, ref) => <span ref={ref} {...props} />);
+
+export const ReactComponent = SvgrMock;
+export default SvgrMock;
