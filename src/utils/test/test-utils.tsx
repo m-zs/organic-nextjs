171e988837f46c4e942c89b787984c8b18a6f242
m-zs <mzasada93@gmail.com>
2022-05-18 22:23:45 +0200

    chore: add localization provider for test utils

--- src/utils/test/test-utils.tsx
+++ src/utils/test/test-utils.tsx
@@ -3,9 +3,19 @@ import "@testing-library/jest-dom";
 import { render, RenderOptions } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "src/styles";
+import { LocalizationProvider } from "src/context/localization";
+import en from "src/content/translations/en.json";
+
+const messages = {
+  en,
+};
 
 const AllTheProviders: FC<{ children: ReactNode }> = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <LocalizationProvider locale={"en"} messages={messages["en"]}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </LocalizationProvider>
+  );
 };
 
 const customRender = (
