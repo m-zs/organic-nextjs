a3539f5a9e6dc51b81f081c4a08995fdb46ed725
m-zs <mzasada93@gmail.com>
2022-05-22 09:39:43 +0200

    chore: add localization provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,15 @@
 import { addDecorator } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 import { RouterContext } from "next/dist/shared/lib/router-context";
+import { LocalizationProvider } from "../src/context/localization";
 import { theme, GlobalStyles } from "../src/styles";
+import en from "../src/content/translations/en.json";
+import pl from "../src/content/translations/pl.json";
+
+const messages = {
+  en,
+  pl,
+};
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,12 +21,15 @@ export const parameters = {
   },
   nextRouter: {
     Provider: RouterContext.Provider,
+    locales: ["en", "pl"],
   },
 };
 
 addDecorator((story) => (
   <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    {story()}
+    <LocalizationProvider locale={"en"} messages={messages["en"]}>
+      <GlobalStyles />
+      {story()}
+    </LocalizationProvider>
   </ThemeProvider>
 ));
