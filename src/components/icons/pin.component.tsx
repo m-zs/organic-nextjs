a57f52f8460813da9924e6fb4eba8fbe70ca75f0
m-zs <mzasada93@gmail.com>
2022-05-22 08:25:40 +0200

    refactor: use component instead of svgr

---
+++ src/components/icons/pin.component.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+
+export const Pin: FC = () => (
+  <svg
+    id="placeholder"
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 21.094 30"
+  >
+    <g id="Group_4" data-name="Group 4">
+      <g id="Group_3" data-name="Group 3">
+        <path
+          id="Path_1"
+          data-name="Path 1"
+          d="M86.547,0a10.549,10.549,0,0,0-8.973,16.093l8.372,13.492a.879.879,0,0,0,.747.415H86.7a.879.879,0,0,0,.747-.427L95.606,15.95A10.549,10.549,0,0,0,86.547,0ZM94.1,15.048,86.68,27.433,79.068,15.167A8.795,8.795,0,1,1,94.1,15.048Z"
+          transform="translate(-76)"
+          fill="#6b6b6b"
+        />
+      </g>
+      <g id="Group_6" data-name="Group 6" transform="translate(5.273 5.273)">
+        <g id="Group_5" data-name="Group 5">
+          <path
+            id="Path_2"
+            data-name="Path 2"
+            d="M171.273,90a5.273,5.273,0,1,0,5.273,5.273A5.279,5.279,0,0,0,171.273,90Zm0,8.8a3.527,3.527,0,1,1,3.521-3.527A3.53,3.53,0,0,1,171.273,98.8Z"
+            transform="translate(-166 -90)"
+            fill="#6b6b6b"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
